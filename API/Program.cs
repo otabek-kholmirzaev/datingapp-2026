8620881cf880e2f8864dbdbbee9f8e29e95b999e
otabek-kholmirzaev <kholmirzaevotabek1@gmail.com>
2026-05-01 21:11:13 +0500

    End of section 10

--- API/Program.cs
+++ API/Program.cs
@@ -37,7 +37,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin()
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseAuthentication();
