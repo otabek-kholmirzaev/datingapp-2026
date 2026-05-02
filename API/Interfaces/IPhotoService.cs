3adcc0bde63effe9caeee10e01ac0a114168f2b5
otabek-kholmirzaev <kholmirzaevotabek1@gmail.com>
2026-05-03 00:56:28 +0500

    End of section 11

---
+++ API/Interfaces/IPhotoService.cs
@@ -0,0 +1,8 @@
+namespace API.Interfaces;
+using CloudinaryDotNet.Actions;
+
+public interface IPhotoService
+{
+    Task<ImageUploadResult> UploadPhotoAsync(IFormFile file);
+    Task<DeletionResult> DeletePhotoAsync(string publicId);
+}
